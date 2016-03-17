094c25e9c3a7069fd5ffaa3e2c03338c90594491
amsibsam <amsibsam@gmail.com>
2016-03-17 17:14:58 +0700

    prepare for login layout

--- app/src/main/java/com/uny/crysdip/ui/activity/LoginActivity.java
+++ app/src/main/java/com/uny/crysdip/ui/activity/LoginActivity.java
@@ -1,15 +1,18 @@
 package com.uny.crysdip.ui.activity;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.uny.crysdip.R;
+import com.uny.crysdip.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
     }
 }
